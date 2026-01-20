3f1e980345c99e079613160b1a3a5a445ea33d64
Ernesto Rojo <gamedev@ernestorojo.com>
2026-01-20 12:57:14 -0600

    Updated main menu to use ui toolkit

--- src/Assets/Scripts/MainMenu.cs
+++ src/Assets/Scripts/MainMenu.cs
@@ -1,11 +1,24 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
 
-public class MainMenu : UIBehaviour
+public class MainMenu : MonoBehaviour
 {
+    private UIDocument _document;
+    private Button _pongButton;
+
+    private void Awake()
+    {
+        _document = GetComponent<UIDocument>();
+
+        _pongButton = _document.rootVisualElement.Q<Button>("startPongGame");
+
+        if (_pongButton != null)
+        {
+            _pongButton.clicked += OnPlayPong;
+        }
+    }
+
     public void OnPlayPong()
     {
         SceneManager.LoadScene("Pong", LoadSceneMode.Single);
