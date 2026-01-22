7c192e42963919d4d3c75712fb576e000fb1a595
Ernesto Rojo <gamedev@ernestorojo.com>
2026-01-21 18:47:50 -0600

    Starting to make doodle jump. Added some art and a scene to start off with

--- src/Assets/Scripts/MainMenu.cs
+++ src/Assets/Scripts/MainMenu.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UIElements;
@@ -6,21 +7,33 @@ public class MainMenu : MonoBehaviour
 {
     private UIDocument _document;
     private Button _pongButton;
+    private Button _doodleJumpButton;
 
     private void Awake()
     {
         _document = GetComponent<UIDocument>();
 
         _pongButton = _document.rootVisualElement.Q<Button>("startPongGame");
+        _doodleJumpButton = _document.rootVisualElement.Q<Button>("startDoodleJumpGame");
 
         if (_pongButton != null)
         {
             _pongButton.clicked += OnPlayPong;
+        }
+
+        if (_doodleJumpButton != null)
+        {
+            _doodleJumpButton.clicked += OnDoodleJump;
         }
     }
 
     public void OnPlayPong()
     {
         SceneManager.LoadScene("Pong", LoadSceneMode.Single);
+    }
+
+    private void OnDoodleJump()
+    {
+        SceneManager.LoadScene("DoodleJump", LoadSceneMode.Single);
     }
 }
